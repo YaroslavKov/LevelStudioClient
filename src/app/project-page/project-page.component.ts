f6ad38e7cb9b971483ad768905d91b720217226f
Yaroslav K <koval.yar.val@gmail.com>
2021-10-08 18:18:21 +0300

    +optimized slider ux +changed beat

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -31,38 +31,72 @@ export class ProjectPageComponent {
         oscillatorNode.connect(this.gainNode);//
 
         oscillatorNode.frequency.value = 150;
-        this.gainNode.gain.value = this.volume?.nativeElement.value;
+        //this.gainNode.gain.value = this.volume?.nativeElement.value;
         //gain.gain.setValueAtTime(1, this._audioContext.currentTime + 1);
 
-        oscillatorNode.start(0);
+        //oscillatorNode.start(0);
         this.gainNode.connect(this._audioContext.destination);
-        oscillatorNode.stop(this._audioContext.currentTime + 0.5);
+        //oscillatorNode.stop(this._audioContext.currentTime + 0.5);
 
-        const beatAudioElement = document.createElement('audio');
-        beatAudioElement.setAttribute('src', this.beatPath);
-        this.beatSource = this._audioContext.createMediaElementSource(beatAudioElement);
-        this.beatSource.connect(this.gainNode);
-        this.gainNode.connect(this._audioContext.destination);
+        if (!this.beatSource) {
+            const beatAudioElement = document.createElement('audio');
+            beatAudioElement.setAttribute('src', this.beatPath);
+            this.beatSource = this._audioContext.createMediaElementSource(beatAudioElement);
+            this.beatSource.connect(this.gainNode);
+            //this.gainNode.connect(this._audioContext.destination);
+        }
         this.beatSource?.mediaElement.play();
 
-        const voiceAudioElement = document.createElement('audio');
-        voiceAudioElement.setAttribute('src', this.voicePath);
-        this.voiceSource = this._audioContext.createMediaElementSource(voiceAudioElement);
-        this.voiceSource.connect(this.gainNode);
-        this.gainNode.connect(this._audioContext.destination);
+        if (!this.voiceSource) {
+            const voiceAudioElement = document.createElement('audio');
+            voiceAudioElement.setAttribute('src', this.voicePath);
+            this.voiceSource = this._audioContext.createMediaElementSource(voiceAudioElement);
+            this.voiceSource.connect(this.gainNode);
+            //this.gainNode.connect(this._audioContext.destination);
+        }
         this.voiceSource?.mediaElement.play();
+
+        const time = this._audioContext.currentTime;
+        //debugger;
     }
 
-    startSound(event: Event) {
+    startSound() {
+        this.changeVolume(this.volume?.nativeElement.value);
         this.playSound();
     }
 
     changeVolume(volume: any) {
-        this.gainNode.gain.value = volume * 2;
+        //this.gainNode.gain.value = volume * 2;
+        volume *= 2;
+        const stepCount = 10;
+        const step = Math.abs(volume - this.gainNode.gain.value) / stepCount;
+        if (step <= 0.001) {
+            return;
+        }
+        let currentValue = this.gainNode.gain.value;
+        const iterations = [];
+        if (volume < currentValue) {
+            while (volume < currentValue) {
+                iterations.push(currentValue);
+                currentValue -= step;
+            }
+        } else {
+            while (volume > currentValue) {
+                iterations.push(currentValue);
+                currentValue += step;
+            }
+        }
+        this.gainNode.gain.setValueCurveAtTime(iterations, this._audioContext.currentTime, 0.2);
     }
 
     stopSound() {
         this.beatSource?.mediaElement.pause();
         this.voiceSource?.mediaElement.pause();
+    }
+
+    resetAudio() {
+        this.stopSound();
+        this.beatSource = undefined;
+        this.voiceSource = undefined;
     }
 }
